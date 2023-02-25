6ee3b2d6213ff75a875ff7be24d414d7a9a7f3c1
DmitryPatskalev <dimanord@yandex.ru>
2023-02-25 12:19:29 +0300

    feat: create test navbar component

--- src/common/navbar/NavBarComponent.tsx
+++ src/common/navbar/NavBarComponent.tsx
@@ -1,17 +1,11 @@
 import React from "react";
 import s from "./NavBar.module.scss";
-import { NavLink } from "react-router-dom";
+import TestComponent from "../TestComponent";
 
 export const NavBarComponent = () => {
   return (
     <div className={s.navbar}>
-      <NavLink to={"login"}>Login</NavLink>
-      <NavLink to={"register"}>Registration</NavLink>
-      <NavLink to={"profile"}>Profile</NavLink>
-      <NavLink to={"*"}>404</NavLink>
-      <NavLink to={"password-recovery"}>Password recovery</NavLink>
-      <NavLink to={"new-password"}>New Password</NavLink>
-      <NavLink to={"stand"}>Stand</NavLink>
+      <TestComponent />
     </div>
   );
 };
