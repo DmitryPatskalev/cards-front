f5f843acb0289372c2970545fcfc4bffde0084c6
DmitryPatskalev <dimanord@yandex.ru>
2023-04-06 15:50:07 +0300

    feat: packs

--- src/common/cards/pack/card/Cards.tsx
+++ src/common/cards/pack/card/Cards.tsx
@@ -3,12 +3,16 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom'
 
 import { SuperButton } from '../../../../components/super-components/button/SuperButton'
+import { SuperDebounceInput } from '../../../../components/super-components/debounce/SuperDebounceInput'
 import style from '../../../auth/profile/Profile.module.scss'
 import common from '../../../common-css-style/common-container.module.scss'
+import table from '../../../common-css-style/Table.module.scss'
 import leftArrow from '../../../utils/img/leftArrow.svg'
 import { SubTitle } from '../../../utils/SubTitle/SubTitle'
 import { Title } from '../../../utils/Title/Title'
 import pack from '../Packs.module.scss'
+
+import s from './Cards.module.scss'
 
 export const Cards = () => {
   const navigate = useNavigate()
@@ -28,6 +32,36 @@ export const Cards = () => {
         <Title title="Friend's Pack" />
         <SuperButton xType="default">Learn to Pack</SuperButton>
       </div>
+
+      <div className={s.searchBlock}>
+        <SubTitle title="Search" />
+        <SuperDebounceInput
+          onChangeText={() => console.log('click')}
+          onDebounceChange={() => console.log('click')}
+          value={''}
+          className={s.searchInputCards}
+          type="text"
+          placeholder="Provide your text"
+        />
+      </div>
+      <table className={table.table}>
+        <thead>
+          <tr>
+            <th>
+              <SubTitle title="Question" />
+            </th>
+            <th>
+              <SubTitle title="Answer" />
+            </th>
+            <th>
+              <SubTitle title="Last Updated" />
+            </th>
+            <th>
+              <SubTitle title="Grade" />
+            </th>
+          </tr>
+        </thead>
+      </table>
     </div>
   )
 }
---
+++ src/common/cards/pack/card/cards-reducer.ts
@@ -0,0 +1,6 @@
+export const cardsReducer = (state: any, action: any) => {
+  switch (action.type) {
+    default:
+      return 'Error'
+  }
+}
